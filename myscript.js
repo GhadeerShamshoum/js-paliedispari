0688450571474a1f6be682e78c6f481138e39deb
Ghadeer <ghaora@gmail.com>
2021-12-17 15:32:08 +0200

    word-reverse

---
+++ myscript.js
@@ -0,0 +1,25 @@
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+function checkPalindroma(word, newStr){
+    let result = false;
+    if(word==newStr){
+        console.log("la parola inserita è palindroma");
+        result = true;
+    }
+    return result;
+}
+
+
+let word = prompt('inserisci una parola');
+
+console.log(word.length);
+let newStr = '';
+
+for(let i=word.length-1; i>=0; i--) {
+    newStr += word[i];
+    
+}
+console.log(newStr);
+
